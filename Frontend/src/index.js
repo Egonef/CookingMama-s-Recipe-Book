abc2749a2b66da82177a060dd2a06baa2a1a6e33
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2024-04-16 00:50:56 +0200

    Un porron de cosas colega

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+
+//routes import
+import Landing from './routes/landing';
+import ErrorPage from './routes/errorPage';
+import Test from './routes/test';
+import RecipesHome from './routes/recipesHome';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+  {
+    path: "/recipesHome",
+    element: <RecipesHome />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ Frontend/src/routes/landing.js
@@ -0,0 +1,64 @@
+//import './Landing.css';
+import { useState } from 'react';
+import Navbar from "./../components/Navbar";
+import CuadroInicio from "./../components/CuadroInicio";
+import Librov4 from "./../components/Librov4";
+import { Canvas } from "@react-three/fiber";
+import '@fontsource/italiana';
+import { useNavigate } from 'react-router-dom';
+
+
+function Landing() {
+	const [showCuadroInicio, setShowCuadroInicio] = useState(true);
+    const navigate = useNavigate();
+
+
+    const hideCuadroInicio = () => {
+        const cuadroInicio = document.getElementById('CuadroTitulo');
+        const divLibro = document.getElementById('divLibro');
+
+        if (cuadroInicio && divLibro) {
+        cuadroInicio.style.transition = 'all 1s ease-out';
+        cuadroInicio.style.transform = 'translateX(-100%)';
+        cuadroInicio.style.opacity = '0';
+
+        divLibro.style.transition = 'all 1s ease-out';
+        divLibro.style.transform = 'translateX(-20%)';
+
+        setTimeout(() => {
+            divLibro.style.scale = '2.7';
+        }, 500);
+
+        setTimeout(() => {
+            divLibro.style.scale = '15';
+        }, 1500);
+
+        setTimeout(() => {
+            setShowCuadroInicio(false);
+            const mainElement = document.getElementById('main');
+            if (mainElement) {
+            mainElement.style.backgroundColor = '#C3B9AB';
+            }
+            divLibro.style.display = 'none';
+            navigate('/recipesHome');
+        }, 2000);
+        }
+    }
+
+	return (
+		<div id='main' className=" h-dvh bg-[#FCF8E8] ">
+			<Navbar />
+			<div id='ContenedorMain' className=" flex flex-row justify-evenly items-center mt-10">
+				{showCuadroInicio && <CuadroInicio />}
+				<div id='divLibro' className='h-[45rem] w-[45rem] '>
+					<Canvas className=" bg-[#FCF8E8]" camera={{ fov: 20, position: [0, 200, 100] }}>
+					<ambientLight intensity={2} />
+					<Librov4  hideCuadroInicio={hideCuadroInicio} />
+					</Canvas>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Landing;
