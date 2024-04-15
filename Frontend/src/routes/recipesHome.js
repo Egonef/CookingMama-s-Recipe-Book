bd1503a2604727f3e39a52e08d59f5c9a6525548
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2024-04-16 00:58:03 +0200

    Animacion bonita al entrar al libro ( Ya si me voy a mimir )

--- Frontend/src/routes/recipesHome.js
+++ Frontend/src/routes/recipesHome.js
@@ -2,14 +2,16 @@ import Navbar from "./../components/Navbar";
 
 export default function RecipesHome() {
     return (
-        <div className="bg-[#C3B9AB] h-dvh">
-            <Navbar />
-            <div className="flex flex-row items-center justify-center mx-16 mt-32">
-                <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-1/3 h-[40rem] mx-2">
-                    <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-12.2rem] bg-[#C3B9AB] px-10">Buscar Recetas</h1>
-                </div>
-                <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-2/3 h-96 mx-2">
-                    <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-12.2rem] bg-[#C3B9AB] px-10">Recetas Populares</h1>
+        <div className="bg-[#C3B9AB] h-dvh ">
+            <div className=" animate-fade-in">
+                <Navbar />
+                <div className="flex flex-row items-center justify-center mx-16 mt-32">
+                    <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-1/3 h-[40rem] mx-2">
+                        <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-12.2rem] bg-[#C3B9AB] px-10">Buscar Recetas</h1>
+                    </div>
+                    <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-2/3 h-96 mx-2">
+                        <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-12.2rem] bg-[#C3B9AB] px-10">Recetas Populares</h1>
+                    </div>
                 </div>
             </div>
         </div>
