6d02394c16c290252f9031ac078a556b56876d22
Egonef <emiliogomeznef@hotmail.com>
2024-04-17 09:44:19 +0200

    AWAKALA

---
+++ Frontend/src/components/RecipeCard.jsx
@@ -0,0 +1,8 @@
+
+export default function RecipeCard() {
+    return (
+        <div className=" bg-orange-200 h-32 w-full my-4 border-r-2">
+            <h1>Recipe Card</h1>
+        </div>
+    )
+}
--- Frontend/src/routes/recipesHome.js
+++ Frontend/src/routes/recipesHome.js
@@ -1,5 +1,5 @@
 import Navbar from "./../components/Navbar";
-
+import RecipeCard from "../components/RecipeCard";
 export default function RecipesHome() {
     return (
         <div className="bg-[#C3B9AB] h-dvh ">
@@ -9,8 +9,14 @@ export default function RecipesHome() {
                     <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-1/3 h-[40rem] mx-2">
                         <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-12.2rem] bg-[#C3B9AB] px-10">Buscar Recetas</h1>
                     </div>
-                    <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-2/3 h-96 mx-2">
-                        <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-12.2rem] bg-[#C3B9AB] px-10">Recetas Populares</h1>
+                    <div className="flex flex-col items-center justify-center border-2 border-[#242925] w-2/3 h-auto mx-2 px-5">
+                        <h1 className="text-4xl font-[italiana] text-[#242925] translate-y-[-2rem] bg-[#C3B9AB] px-10">Recetas Populares</h1>
+                        <div className="flex flex-col items-center justify-center w-full">
+                            <RecipeCard />
+                            <RecipeCard />
+                            <RecipeCard />
+
+                        </div>
                     </div>
                 </div>
             </div>
