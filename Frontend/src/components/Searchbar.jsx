0df433d667f87643a83212580677212ce0d78f8a
Alicia <agaravi3535@gmail.com>
2024-05-29 20:00:25 +0200

    Searchbar enableada

--- Frontend/src/components/Searchbar.jsx
+++ Frontend/src/components/Searchbar.jsx
@@ -1,12 +1,18 @@
 import { motion } from "framer-motion"
 
-export default function Searchbar() {
+export default function Searchbar ({ searchTerm, setSearchTerm }) {
     return (
         <motion.div whileHover={{ scale: 1.07 }} className="flex flex-row justify-left items-center bg-orange-100 h-10 w-96 my-4 px-3 rounded-full">
-            <motion.input className=" bg-transparent outline-none h-19 w-96 rounded-full"/>
-            <motion.button className=" bg-transparent h-8 w-8 rounded-full ml-2">
+            <motion.input 
+                className="bg-transparent outline-none h-19 w-96 rounded-full" 
+                type="text"
+                placeholder="Search for recipes..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <motion.button className="bg-transparent h-8 w-8 rounded-full ml-2">
                 <img src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png" alt="buscar"/>
             </motion.button>
         </motion.div>
-    )
+    );
 }
--- Frontend/src/routes/recipesHome.js
+++ Frontend/src/routes/recipesHome.js
@@ -3,25 +3,43 @@ import RecipeCard from "../components/RecipeCard";
 import Searchbar from "../components/Searchbar";
 import recipeData from "../components/dabate.json";
 import { useState , useEffect } from "react";
+import axios from 'axios';
 
 
 export default function RecipesHome() {
 
     const [numRecipes , setNumRecipes] = useState(0);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+
 
     useEffect(() => {
-        setNumRecipes(recipeData.Recipe.length);
-    }, []);
+        const fetchRecipes = async () => {
+            try {
+                const response = await axios.get(`http://localhost:5000/api/recipes/ingredients?ingredients=${searchTerm}`);
+                setFilteredRecipes(response.data);
+            } catch (error) {
+                console.error("Error fetching recipes:", error);
+            }
+        };
+    
+        if (searchTerm) {
+            fetchRecipes();
+        } else {
+            // Si no hay término de búsqueda, puedes mostrar todas las recetas o limpiar la lista
+            setFilteredRecipes([]);
+        }
+    }, [searchTerm]);
 
     return (
         <div className="bg-[#C3B9AB]">
             <div className=" animate-fade-in">
                 <Navbar />
                 <div className="flex flex-row items-center justify-left mx-16">
-                    <Searchbar />
+                <Searchbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </div>
                 <div className="flex flex-row justify-evenly items-center flex-wrap gap-x-1 gap-y-4 mt-16 mx-16">
-                    {Array.from({length: numRecipes}, (_, i) => <RecipeCard key={i} recipeNumber={i} />)}
+                    {filteredRecipes.map((recipe, i) => <RecipeCard key={i} recipe={recipe} />)}
                 </div>
             </div>
         </div>
